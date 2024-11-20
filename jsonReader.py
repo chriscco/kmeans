3d10412aaf0b99df1644d16a512f68dd2b637553
chriscco <2444628489c@gmail.com>
2024-11-19 20:15:43 -0800

    fixed bug

--- jsonReader.py
+++ jsonReader.py
@@ -2,9 +2,8 @@ import json
 
 
 class JsonRead:
-
     __conversation_path = "files/conversation_graphs.json"
-    __proximity_path = "files/proximit_graphs.json"
+    __proximity_path = "files/proximity_graphs.json"
     __attention_path = "files/shared_attention_graphs.json"
 
     __conversation_data = [];
@@ -17,25 +16,29 @@ class JsonRead:
             data = json.load(f)
 
         if filename == self.__conversation_path:
-            self.__conversation_data = data
+            self.__conversation_data = data["graphs"]
         elif filename == self.__attention_path:
-            self.__attention_data = data
-        else: self.__proxi_data = data
+            self.__attention_data = data["graphs"]
+        else: self.__proxi_data = data["graphs"]
 
-        return data
+        return data["graphs"]
 
     def get_conversation(self):
-        if self.__conversation_path is not None:
+        if self.__conversation_path != []:
             return self.__conversation_data
         else: return self.reader(self.__conversation_path)
 
     def get_attention(self):
-        if self.__attention_data is not None:
+        if self.__attention_data != []:
             return self.__attention_data
         else: return self.reader(self.__attention_path);
 
-
     def get_proximity(self):
-        if self.__proxi_data is not None:
+        if self.__proxi_data != []:
             return self.__proxi_data
         else: return self.reader(self.__proximity_path);
+
+
+if __name__ == "__main__":
+    j = JsonRead()
+    print(j.get_proximity())
